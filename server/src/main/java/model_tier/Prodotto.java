88abbfedf86e35aeb1cf9c7208f77ca532b41133
Andrea Paciocca <81762546+AndreaPac@users.noreply.github.com>
2021-07-16 11:21:50 +0200

    aggiunti @Column

--- server/src/main/java/model_tier/Prodotto.java
+++ server/src/main/java/model_tier/Prodotto.java
@@ -1,5 +1,6 @@
 package model_tier;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -10,7 +11,9 @@ public class Prodotto {
 	@Id
 	@GeneratedValue
 	private Long id;
+	@Column
 	private String nome;
+	@Column
 	private double prezzo;
 
 	public Prodotto() {
