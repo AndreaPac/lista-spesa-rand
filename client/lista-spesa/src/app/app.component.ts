0dbc08a7d43ee971ccbbc1a978db879eb88a5e7f
debora-98 <debsortini23@gmail.com>
2021-07-16 10:59:46 +0200

    grafica

--- client/lista-spesa/src/app/app.component.ts
+++ client/lista-spesa/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Prodotto } from './prodotto';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lista-spesa';
+  prodotto : Prodotto = new Prodotto();
+  prodotti : Prodotto[]= [{nome:"mela"}];
+
+  aggiungi(){
+
+  }
+
+  rimuovi(){
+
+  }
 }
